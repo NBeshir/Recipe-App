27f37e00437d8b65d421d09da67aeec98ccd4968
Nejmiya Beshir <tnejmi@gmail.com>
2021-03-10 22:08:56 -0800

    initial commit

---
+++ shared/recipe.js
@@ -0,0 +1,68 @@
+export const RECIPES = [
+  {
+    id: 0,
+    name: "Thai Food",
+    image:
+      "https://images.unsplash.com/photo-1613276016849-469ca6b326bd?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OXx8Zm9vZCUyMHRoYWl8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+
+    description: "A very delicious food that you will enjoy",
+  },
+  {
+    id: 1,
+    name: "Taiwanese Food ",
+    image:
+      "https://images.unsplash.com/photo-1613276016849-469ca6b326bd?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OXx8Zm9vZCUyMHRoYWl8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+
+    description: "A very delicious food that you will enjoy",
+  },
+  {
+    id: 2,
+    name: "American Food",
+    image:
+      "https://images.unsplash.com/photo-1613276016849-469ca6b326bd?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OXx8Zm9vZCUyMHRoYWl8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+
+    description: "A very delicious food that you will enjoy",
+  },
+
+  {
+    id: 3,
+    name: "Ethiopian Food",
+    image:
+      "https://images.unsplash.com/photo-1613276016849-469ca6b326bd?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OXx8Zm9vZCUyMHRoYWl8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+
+    description: "A very delicious food that you will enjoy",
+  },
+  {
+    id: 4,
+    name: "Indian Food",
+    image:
+      "https://images.unsplash.com/photo-1613276016849-469ca6b326bd?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OXx8Zm9vZCUyMHRoYWl8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+
+    description: "A very delicious food that you will enjoy",
+  },
+  {
+    id: 5,
+    name: "Danish Food ",
+    image:
+      "https://images.unsplash.com/photo-1613276016849-469ca6b326bd?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OXx8Zm9vZCUyMHRoYWl8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+
+    description: "A very delicious food that you will enjoy",
+  },
+  {
+    id: 6,
+    name: "Canadian Food",
+    image:
+      "https://images.unsplash.com/photo-1613276016849-469ca6b326bd?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OXx8Zm9vZCUyMHRoYWl8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+
+    description: "A very delicious food that you will enjoy",
+  },
+
+  {
+    id: 7,
+    name: "Japanese Food",
+    image:
+      "https://images.unsplash.com/photo-1613276016849-469ca6b326bd?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OXx8Zm9vZCUyMHRoYWl8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+
+    description: "A very delicious food that you will enjoy",
+  },
+];
